354447dd52a611b7babc1be96b6c75ea051e7d11
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-12-06 23:40:39 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
-
 #include "btree.h"
-
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    system("pause");
-    return EXIT_SUCCESS;
+int main() {
+BTree<int> arbol(3);
+
+arbol.insert(2);
+arbol.insert(0);
+arbol.insert(5);
+
+
+arbol.print();
+
+
+    if(arbol.find(5)){
+        cout<<"encontrado"<<endl;
+
+    } else{
+        cout<<"no"<<endl;
+    }
 }
